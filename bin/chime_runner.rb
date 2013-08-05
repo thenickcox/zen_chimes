856fc8cf997b59410a994090673485768c392b7e
Winfield Peterson <winfield.peterson@gmail.com>
2013-08-05 10:06:26 -0400

    First pass at command wrapper and chime samples.

---
+++ bin/chime_runner.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+CHIME_DIR = File.realpath File.dirname(__FILE__) + '/../audio'
+puts "Audio Path: #{CHIME_DIR}"
+require 'time'
+
+def play_chime(style='single')
+  system "afplay #{CHIME_DIR}/chime_#{style}.mp3 >/dev/null &"
+end
+
+start = Time.now
+play_chime('single')
+
+puts "=[Zen Chimes]=================================================="
+puts "= Running: '#{ARGV.join(' ')}'"
+puts "= "
+puts "= Take this time to breath and focus."
+puts "==============================================================="
+puts ""
+
+system *Array( ARGV )
+
+puts ""
+puts "==============================================================="
+puts ""
+
+elapsed_time = Time.now - start
+# Wait at least 10 seconds between start/end chimes
+sleep (5-elapsed_time) if elapsed_time < 5
+
+if elapsed_time < 60
+  play_chime('single')
+else
+  play_chime('triple')
+end
